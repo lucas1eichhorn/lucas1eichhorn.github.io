27e91827cb85ebd33290da7be6e526643bec6fda
Lucas Eichhorn <lucas1eichhorn@gmail.com>
2025-03-02 13:35:29 +0100

    add metadata

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,37 @@ import "./globals.css";
 import Providers from "./providers";
 
 export const metadata = {
-    title: "Lucas Eichhorn | Software Data Engineer",
+  "title": "Lucas Eichhorn | Data and Software Engineer",
+  "description": "Portfolio of Lucas Eichhorn, a Data and Software Engineer with experience in Big Data, Machine Learning, and Software development. Based in Madrid, Spain, specializing in innovative technological solutions and business process optimization.",
+  "keywords": [
+    "Lucas Eichhorn",
+    "Data Engineer",
+    "Software Engineer",
+    "Big Data",
+    "Machine Learning",
+    "Cloud Computing",
+    "Data Science",
+    "Madrid",
+    "Spain",
+    "technological solutions",
+    "process optimization"
+  ],
+  "openGraph": {
+    "title": "Lucas Eichhorn | Data and Software Engineer",
+    "description": "Explore the portfolio of Lucas Eichhorn, an expert in Big Data, Machine Learning, and Software development, offering innovative technological solutions from Madrid, Spain.",
+    "image": "https://lucaseichhorn.com.ar/images/slider/person.png",
+    "url": "https://lucaseichhorn.com.ar",
+    "type": "website"
+  },
+  "twitter": {
+    "card": "summary_large_image",
+    "title": "Lucas Eichhorn | Data and Software Engineer",
+    "description": "Meet Lucas Eichhorn, a specialist in Big Data, Machine Learning, and Software development, providing technological solutions from Madrid, Spain.",
+    "image": "https://lucaseichhorn.com.ar/images/slider/person.png"
+  },
+  "robots": "index, follow",
+  "canonical": "https://lucaseichhorn.com.ar",
+  "favicon": "/favicon.ico"
 };
 
 export default function RootLayout({ children }) {
